72c3fa6e7efe23ef6e6773ee857a57d02c8d0522
alfredjo00 <69204159+alfredjo00@users.noreply.github.com>
2022-01-29 22:34:50 +0100

    Update network-extragraphs.py

--- network-extragraphs.py
+++ network-extragraphs.py
@@ -18,19 +18,21 @@ from utils.help_methods import cartesian_to_spherical
 
 ## for extra graphs
 
-import pandas as pd
-import scipy.optimize as opt
-import datashader as ds
-import datashader.transfer_functions as tf
+from pandas import DataFrame
+from datashader import Canvas
+from datashader.transfer_functions import stack
+from datashader.transfer_functions import shade
+from datashader.transfer_functions import Image
+from datashader.utils import export_image
+from scipy.stats import pearsonr
+from scipy.optimize import curve_fit
 import numpy as np
 import matplotlib.pyplot as plt
 import colorcet as cc
-from scipy.stats import pearsonr
-from datashader.utils import export_image
 
 ## ----------- Load and prepare data for training/evaluation -------------------
-
-NPZ_DATAFILE = os.path.join(os.getcwd(), 'data', 'XB_mixed_data_e7.npz')
+datafile = 'XB_e6_iso_0_10MeV_gamma_numcl10.npz'
+NPZ_DATAFILE = os.path.join(os.getcwd(), 'data', datafile)
 
 TOTAL_PORTION = 1      #portion of file data to be used, (0,1]
 EVAL_PORTION = 0.2      #portion of total data for final evalutation (0,1)
@@ -62,8 +64,8 @@ model.compile(optimizer=Adam(lr=LEARNING_RATE), loss=loss.get(), metrics=['accur
 ## ----------------- Train the neural network and plot results -----------------
 
 #train the network with training data
-NO_EPOCHS = 10          # no. times to go through training data
-BATCH_SIZE = 2**9       # the training batch size
+NO_EPOCHS = 10         # no. times to go through training data
+BATCH_SIZE = 2**10       # the training batch size
 training = model.fit(train, train_,
                      epochs=NO_EPOCHS,
                      batch_size=BATCH_SIZE,
@@ -79,125 +81,120 @@ predictions = model.predict(eval)
 # return the combination that minimized the loss function (out of max_mult! possible combinations)
 predictions, eval_ = get_permutation_match(predictions, eval_, loss, max_mult)
 
-# plot the "lasersvärd" in spherical coordinates (to compare with previous year)
+# plot the "lasersv???rd" in spherical coordinates (to compare with previous year)
 predictions = cartesian_to_spherical(predictions, error=True)
 eval_ = cartesian_to_spherical(eval_, error=True)
 figure, rec_events = plot_predictions(predictions, eval_, show_detector_angles=True)
 
 # save figures and trained parameters
 
-Direc = 'ann-graphs'
+Direc = 'ann-graphs' 
 save(Direc, figure, learning_curve, model)
 
 
 ## ----------- for extra graphs --------------------
-
-
-# get directory
-
+  
+# get the most recently created 'Direc' directory 
 all_subdirs = [d for d in os.listdir('.') if os.path.isdir(d)]
 exmps = [d for d in all_subdirs if Direc in d]
-latest_exmp = max(exmps, key=os.path.getmtime)
+recent_dir = max(exmps, key=os.path.getmtime)
 #
-x = lambda n: eval_[:,n] # x(0,1,2) eval of energy,theta,phi -- simulated / 'correct'
-y = lambda n: predictions[:,n] # ANN generated predictions
 
-# least squares for E , theta, phi
+# Sometimes eval_ and predictions gets split up into multiple arrays, needs to 
+# concat them before graphing them
+N = int(len(eval_[0,:])/3) #amount of times it split
+
+# x(n) 'correct', eval data: 0->E, 1->Theta, 2->Phi.
+# y(n) 'reconstructed', predicted data: 0->E, 1->Theta, 2->Phi.
+
+
+def x_eval(n): 
+    X = [None]*N
+    for i in range(N):
+        X[i] = eval_[:,n + 3* i]
+    return np.concatenate([i for i in X])
+
+def y_pred(n):
+    Y = [None]*N
+    for i in range(N):
+        Y[i] = predictions[:,n + 3* i]
+    return np.concatenate([i for i in Y])
+
+# least squares for E, theta, phi
 
 def func(x,A): #function to curve fit, want A to be ~1
-    return x*A
-## cut off too high E values because for some reason prediction
-## of E is capped at 20 MeV when evals can be higher.
-E = 10
-#Energy limit for least squares (MeV),
-#set E to equal event_recon graph. Only affects curve fit, covariance
-list_pred=list(y(0)) # Y
-list_eval_=list(x(0)) # X
+    return x*A 
+## cut off too high E values
+E_max = np.max(x_eval(0))
+E_min = min(i for i in x_eval(0) if i > 0)
 
-i=0
-while i<len(list_pred): ## any predicted or correct E pair of values
+list_pred=list(y_pred(0)) # Y
+list_eval_=list(x_eval(0)) # X
+
+i=0                     ## Makes it so maximum of x and y are the same
+while i<len(list_pred): ## any predicted or correct E pair of values 
                         ## (for E> something)gets deleted from array
-    if list_pred[i] > E or list_eval_[i] > E:
+    if list_pred[i] > E_max or list_eval_[i] > E_max:        
         del list_eval_[i]
         del list_pred[i]
     else:
-        i+=1
-####
-w_E, cov_E = opt.curve_fit(func, list_eval_,list_pred,  p0=1)
-w_T, cov_T = opt.curve_fit(func, x(1), y(1), p0=1)
-w_P, cov_P = opt.curve_fit(func, x(2), y(2), p0=1)
-corr_E, _ = pearsonr(list_eval_,list_pred)
-corr_T, _ = pearsonr(x(1), y(1))
-corr_P, _ = pearsonr(x(2), y(2))
+        i+=1   
+        
+####       
+w_E, _ = curve_fit(func, list_eval_,list_pred,  p0=1) 
+w_T, _ = curve_fit(func, x_eval(1), y_pred(1), p0=1)
+w_P, _ = curve_fit(func, x_eval(2), y_pred(2), p0=1) 
 
+corr_E, _ = pearsonr(list_eval_,list_pred) 
+corr_T, _ = pearsonr(x_eval(1), y_pred(1)) 
+corr_P, _ = pearsonr(x_eval(2), y_pred(2))
 
-tmpstr= ["E slope:" + str(w_E) + ". E cov: " + str(cov_E), "Theta slope:"+ str(w_T) +\
-         ". Theta covariance:" + str(cov_T), "Phi slope:" + str(w_P) + ". Phi cov:" + str(cov_P),\
-         "E Pearson cor: " + str(corr_E), "Theta Pearson cor: " + str(corr_T), "Phi Pearson cor: " + str(corr_P)]
-np.savetxt(latest_exmp+"/lsq.txt", tmpstr, fmt='%s') #Curve fit txt file,
+tmpstr= ["Datafile used: "+ datafile + ", E min: " + str(E_min) +\
+        ", E max: " + str(E_max), "E slope:" + str(w_E) ,\
+        "Theta slope:"+ str(w_T), "Phi slope:" + str(w_P),"E Pearson cor: " +\
+        str(corr_E),"Theta Pearson cor: " + str(corr_T), "Phi Pearson cor: " +\
+        str(corr_P)]
+print(tmpstr)
+np.savetxt(recent_dir+"/info.txt", tmpstr, fmt='%s') #data info txt file,
 
-## For the 3 seperate graphs
-
+## For the 3 seperate graphs 
 
 ##
 
-df0 = pd.DataFrame(data=np.column_stack((x(0),y(0))),columns=['colX','colY']) #Energy
-df1 = pd.DataFrame(data=np.column_stack((x(1),y(1))),columns=['colX','colY']) #Theta
-df2 = pd.DataFrame(data=np.column_stack((x(2),y(2))),columns=['colX','colY']) #Phi
+N_px = 400 # Square plot width/height px
+max0 = [E_max, np.pi, 2*np.pi]
+
+df_l = [DataFrame(data=np.column_stack((x_eval(i),y_pred(i))),columns=['colX','colY']) for i in [0,1,2]]
+
+i_l = [Image(shade(Canvas(plot_width=N_px, plot_height=N_px, x_range=(0,max0[i]), y_range=(0,max0[i]),
+                   x_axis_type='linear', y_axis_type='linear').points(df_l[i],'colX','colY'), cmap=cc.fire)) for i in [0,1,2]]
 
 
-N = 450 # Square plot width/height px
-E = 10 #np.max(eval_[:,0])*0.75 # X,Y range of axis,in MeV
-E_min = np.min(eval_[:,0])
-
-i0 = tf.Image(tf.shade(ds.Canvas(plot_width=N, plot_height=N, x_range=(0,E), y_range=(0,E),
-                   x_axis_type='linear', y_axis_type='linear').points(df0,'colX','colY'), cmap=cc.fire))
-
-i1 = tf.Image(tf.shade(ds.Canvas(plot_width=N, plot_height=N, x_range=(0,3.1415), y_range=(0,3.1415),
-                   x_axis_type='linear', y_axis_type='linear').points(df1,'colX','colY'), cmap=cc.fire))
-
-i2 = tf.Image(tf.shade(ds.Canvas(plot_width=N, plot_height=N, x_range=(0,2*3.1415), y_range=(0,2*3.1415),
-                   x_axis_type='linear', y_axis_type='linear').points(df2,'colX','colY'), cmap=cc.fire))
-
-dftmp = pd.DataFrame(data=np.column_stack((np.linspace(0,E,1000),np.linspace(0,E,1000))),columns=['x','y']) ##
-line = tf.Image(tf.shade(ds.Canvas(plot_width=N, plot_height=N, x_range=(0,E), y_range=(0,E),               ##
+dftmp = DataFrame(data=np.column_stack((np.linspace(0,E_max,1000),np.linspace(0,E_max,1000))),columns=['x','y']) ##
+line = Image(shade(Canvas(plot_width=N_px, plot_height=N_px, x_range=(0,E_max), y_range=(0,E_max),               ##
                    x_axis_type='linear', y_axis_type='linear').points(dftmp,'x','y')))                      ##
 
 
+file_name = ['/img-E-'+str(np.round(E_min)) + '-'+ str(np.round(E_max))+'MeV', '/img-Theta',\
+             '/img-Phi' ]
+im_name = ['E', 'Theta', 'Phi']
 
-export_image(tf.stack(i0,line, name="E"),filename= latest_exmp +'/img-E-' + \
-             str(np.round(E_min)) + '-'+ str(np.round(E))+"MeV", background="black")
-export_image(tf.stack(i1,line, name="Theta"),filename=latest_exmp +'/img-Theta', background="black")
-export_image(tf.stack(i2,line, name="Phi"),filename=latest_exmp +'/img-Phi', background="black")
-## ------ 2nd part of graphs -------------
-#np.max(x(0))*0.75
-max0 = 10  #chosen to match covariance range and event_recon range
-max1 = 3.1415
-max2 = 3.1415 * 2
+[export_image(stack(i_l[i],line, name=im_name[i]),filename=recent_dir +file_name[i],\
+              background="white") for i in [0,1,2] ]
+
+## --------The three hexagonal bin graphs-------------
+
+plot_name = ["Energy: " + str(np.round(E_min)) + " - " + str(np.round(E_max)), "Theta", "Phi"]
 
 fig, axs = plt.subplots(ncols=3, sharey=False, figsize=(25, 7))
 fig.subplots_adjust(hspace=0.5, left=0.07, right=0.93)
 
+for i in range(3):
+    ax = axs[i]
+    hb = ax.hexbin(x_eval(i), y_pred(i), gridsize=50, bins='log', cmap='inferno')
+    ax.axis([0, max0[i], 0, max0[i]])
+    ax.set_title(plot_name[i])
+    cb = fig.colorbar(hb, ax=ax)
+    cb.set_label('log10(N)')
 
-ax = axs[0]
-hb = ax.hexbin(x(0), y(0), gridsize=50, bins='log', cmap='inferno')
-ax.axis([0, max0, 0, max0])
-ax.set_title("E")
-cb = fig.colorbar(hb, ax=ax)
-cb.set_label('log10(N)')
-
-ax = axs[1]
-hb = ax.hexbin(x(1), y(1), gridsize=50, bins='log', cmap='inferno')
-ax.axis([0, max1, 0, max1])
-ax.set_title("Theta")
-cb = fig.colorbar(hb, ax=ax)
-cb.set_label('log10(N)')
-
-ax = axs[2]
-hb = ax.hexbin(x(2), y(2), gridsize=50, bins='log', cmap='inferno')
-ax.axis([0, max2, 0, max2])
-ax.set_title("Phi")
-cb = fig.colorbar(hb, ax=ax)
-cb.set_label('log10(N)')
-
-plt.savefig(latest_exmp+"/img-binsETP.png")
+plt.savefig(recent_dir+"/img-binsETP.png")
