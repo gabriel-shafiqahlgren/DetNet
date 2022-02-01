3bb77bb537e58695c24e63e564ecc00edeb4890d
alfredjo00 <69204159+alfredjo00@users.noreply.github.com>
2022-02-01 13:07:16 +0100

    Added  plot_predictions_bar function
    Similar to plot_predictions, adds two bars for energies 0 < E < 0.1 MeV.

--- utils/plot_methods.py
+++ utils/plot_methods.py
@@ -345,3 +345,119 @@ class Arrow3D(FancyArrowPatch):
 
 
 
+#scatter plot ('lasersvärd' graph) plus one vertical and one horizontal bar of energies near 0
+def plot_predictions_bar(y, y_, bins=500, show_detector_angles=True):
+    
+    # Equation system for left, bottom bar of graph. 
+    # a * min_eval_ + b =  0
+    # a * epsilon + b = depth
+    # a,b unknown.
+
+    # 'epsilon': The threshold of energies to look into. [0 , epsilon] interval of energies is represented in the bar.
+    # 'depth': Width of the bar essentially. Around [1 , 2] is good.
+    
+    epsilon = 0.1
+    depth = -1.35
+
+    min_eval_ = min(y_[::,0::3].flatten())
+    min_pred = min(y[::,0::3].flatten())
+
+    A1 = np.array([[min_eval_,1],[epsilon,1]])
+    A2 = np.array([[min_pred,1],[epsilon,1]])
+    v = np.array([0, depth])
+
+    a1,b1 = np.linalg.solve(A1,v)
+    a2,b2 = np.linalg.solve(A2,v)
+
+    # Lowest value of 'correct_energy' is ~ 0.05
+    # For leftbar: Intension is to map the lowest 'x, y_, eval_' value to 0 and highest to 'depth'. 'y, predictions' values remain unchanged
+    # For bottombar: Maps the lowest value to 0 and the highest to 'depth'.
+
+    Y_leftbar = np.array([y[::,0::3].flatten()[i] for i, e in enumerate(y_[::,0::3].flatten()) if e < epsilon])
+    X_leftbar = np.array([a1*y_[::,0::3].flatten()[i]+b1 for i, e in enumerate(y_[::,0::3].flatten()) if e < epsilon])
+
+    Y_botbar = np.array([a2*y[::,0::3].flatten()[i]+b2 for i, e in enumerate(y[::,0::3].flatten()) if e < epsilon])
+    X_botbar = np.array([y_[::,0::3].flatten()[i] for i, e in enumerate(y[::,0::3].flatten()) if e < epsilon])
+
+    events = {'predicted_energy': np.concatenate([y[::,0::3].flatten(), Y_leftbar, Y_botbar]),
+              'correct_energy': np.concatenate([y_[::,0::3].flatten(), X_leftbar, X_botbar]), 
+
+              'predicted_theta': y[::,1::3].flatten(),
+              'correct_theta': y_[::,1::3].flatten(),
+
+              'predicted_phi': np.mod(y[::,2::3], 2*np.pi).flatten(),
+              'correct_phi': y_[::,2::3].flatten()}
+    
+    fig, axs = plt.subplots(1,3, figsize=(20, 8))
+    colormap = truncate_colormap(plt.cm.afmhot, 0.0, 1.0)
+    img = []
+    img.append(axs[0].hist2d(events['correct_energy'], events['predicted_energy'],cmap=colormap, bins=bins, norm=LogNorm()))
+    img.append(axs[1].hist2d(events['correct_theta'], events['predicted_theta'], cmap=colormap, bins=bins, norm=LogNorm()))
+    img.append(axs[2].hist2d(events['correct_phi'], events['predicted_phi'], cmap=colormap, bins=bins, norm=LogNorm()))
+
+    max_energy = 10
+    max_theta = np.pi
+    max_phi = 2*np.pi
+    line_color = 'blue'
+
+    line = np.linspace(0,max_energy)
+    for i in range(0,3):
+        axs[i].plot(line,line, color=line_color, linewidth = 2, linestyle = '-.')
+
+    hline = np.linspace(depth,max_energy)
+    axs[0].plot(np.zeros(50), hline, color=line_color, linewidth = 2)
+    axs[0].plot(hline, np.zeros(50), color=line_color, linewidth = 2)
+
+
+    if show_detector_angles:
+        detector_theta, detector_phi = get_detector_angles()
+        axs[1].scatter(detector_theta, detector_theta, marker='x')
+        axs[2].scatter(detector_phi, detector_phi, marker='x')
+
+
+    axs[0].set_xlabel('Correct E [MeV]', fontsize = TEXT_SIZE)
+    axs[1].set_xlabel('Correct \u03F4', fontsize = TEXT_SIZE)
+    axs[2].set_xlabel('Correct \u03A6', fontsize = TEXT_SIZE)
+    axs[0].set_ylabel('Reconstructed E [MeV]', fontsize = TEXT_SIZE)
+    axs[1].set_ylabel('Reconstructed \u03F4', fontsize = TEXT_SIZE)
+    axs[2].set_ylabel('Reconstructed E \u03A6', fontsize = TEXT_SIZE)
+
+
+    axs[0].set_xlim([depth, max_energy])
+    axs[0].set_ylim([depth, max_energy])
+    axs[0].set_aspect('equal', 'box')
+    axs[1].set_xlim([0, max_theta])
+    axs[1].set_ylim([0, max_theta])
+    axs[1].set_aspect('equal', 'box')
+    axs[2].set_xlim([0, max_phi])
+    axs[2].set_ylim([0, max_phi])
+    axs[2].set_aspect('equal', 'box')
+
+    cb1 = fig.colorbar(img[0][3], ax = axs[0], fraction=0.046, pad = 0.04)
+    # fig.delaxes(cb1.ax)
+    cb2 = fig.colorbar(img[1][3], ax = axs[1], fraction=0.046, pad = 0.04)
+    # fig.delaxes(cb2.ax)
+    cb3 = fig.colorbar(img[2][3], ax = axs[2], fraction=0.046, pad=0.04)
+
+    cb1.ax.tick_params(labelsize = TEXT_SIZE)
+    cb2.ax.tick_params(labelsize = TEXT_SIZE)
+    cb3.ax.tick_params(labelsize = TEXT_SIZE)
+
+    axs[0].tick_params(axis='both', which='major', labelsize=TEXT_SIZE)
+    axs[1].tick_params(axis='both', which='major', labelsize=TEXT_SIZE)
+    axs[2].tick_params(axis='both', which='major', labelsize=TEXT_SIZE)
+
+    plt.sca(axs[0])
+    plt.xticks(np.linspace(0, 10, 6),['0','2','4','6','8','10'])
+    plt.yticks(np.linspace(0, 10, 6),['0','2','4','6','8','10'])
+
+    plt.sca(axs[1])
+    plt.xticks(np.linspace(0, np.pi, 3),['0','$\pi/2$','$\pi$'])
+    plt.yticks(np.linspace(0, np.pi, 3),['0','$\pi/2$','$\pi$'])
+
+    plt.sca(axs[2])
+    plt.xticks(np.linspace(0, 2*np.pi, 3),['0','$\pi$','$2\pi$'])
+    plt.yticks(np.linspace(0, 2*np.pi, 3),['0','$\pi$','$2\pi$'])
+
+    fig.tight_layout()
+    return fig, events
