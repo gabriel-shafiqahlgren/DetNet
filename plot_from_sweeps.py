6979dc68ab0730142abb7cc565647cbbfc07d814
alfredjo00 <alfredjo00@gmail.com>
2022-03-08 17:47:47 +0100

    Small improvement.

--- plot_from_sweeps.py
+++ plot_from_sweeps.py
@@ -5,36 +5,38 @@ Created on Wed Feb 16 13:11:27 2022
 
 Script that loads the file saved with parameter sweep.
 
-To implement:
-    Load a dict instead of a numpy array since one now has to memorize what
-    column contains what :P
-
 @author: gabahl
 """
 
-
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
+TEXT_SIZE = 15
 
-#get the ttl_port figures.
-#Check firsts row of file for exact parameter values.
-arr = np.loadtxt('ann_iterations/lr_rate_sweep3/data_matrix.csv', delimiter=',', skiprows=1)
+font = {'family' : 'STIXGeneral',
+        'weight' : 'normal',
+        'size'   : TEXT_SIZE}
+plt.rc('font', **font)
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams['font.family'] = 'STIXGeneral'
+
+df = pd.read_csv("./DetNet/ann_iterations/parasweepnm/data_matrix.csv")
+df_to_doct = df.to_dict()
+
+x = 'Patience' # Key from cvs file to have as x axis
+y = 'Mean of mean momentum error' # Use as key to 
+yerr = 'Std of mean momentum error'
+
+ls_x = df_to_doct[x].values()
+ls_y = df_to_doct[y].values()
+ls_yerr = df_to_doct[yerr].values()
+
+label = 'w128 d5' # Legend label; network specifics 
 plt.rcParams["figure.figsize"] = (14,8)
+plt.errorbar(ls_x, ls_y, yerr=ls_yerr, label=label)
 
-
-lr_rate = np.log10(arr[:,9]) 
-mean_MME4_128 = arr[:, 3]
-std_MME4_128 = arr[:, 4]
-
-lw = 2
-plt.errorbar(lr_rate, mean_MME4_128, yerr=std_MME4_128, label='w128 d4')
-
-
+plt.grid() # Optional
 plt.legend()
-plt.xlabel("$log_{10}(learning rate)$")
-plt.ylabel("mean MME (N=3)")
-
-plt.show()
-
-
+plt.xlabel(x)
+plt.ylabel(y + ' (N =  3)')
+plt.savefig(x+'.png')
