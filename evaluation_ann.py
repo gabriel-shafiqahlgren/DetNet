2c247c4d3b7e02faa596ef772afd45937d6a49dc
Björn Johansson <bjoj@alvis2.int.private>
2022-05-05 17:25:21 +0200

    It now iterates alphanumerically over all files. Doesn't load the models properly.

--- evaluation_ann.py
+++ evaluation_ann.py
@@ -10,7 +10,9 @@ Comment> Would be alot easier if the models were saved rather than their weights
 """
 import os
 import numpy as np
+import glob as glob
 
+from tensorflow import keras
 from tensorflow.keras.optimizers import Adam
 from utils.models import FCN, Dense_ResNet
 
@@ -24,6 +26,7 @@ from utils.help_methods import get_momentum_error_dist, cartesian_to_spherical, 
 from utils.help_methods import spherical_to_cartesian, get_permutation_match_with_permutations
 
 from addback import addback
+from addback import reshapeArrayZeroPadding
 
 def main():
     PORTION_ZEROS = 0.05
@@ -34,12 +37,14 @@ def main():
     network_type = NETWORK_TYPES['FCN']
 
     # The directory where all weights are stored for FCNs
-    evaluation_directory = 'network_parameters'
-    weight_files = Path(evaluation_directory).glob('*.h5')
+    model_directory = '/cephyr/NOBACKUP/groups/snic2022-5-74/FCN_sweeps/DetNet/saved_models/'
 
     # The directory where evaluation files are stored
     data_directory = 'evaluation_files'
-    data_files = Path(data_directory).glob('*.npz')
+    data_files = Path(data_directory).rglob('*.npz')
+    data_dir = glob.glob(os.path.join(data_directory, '*.npz'))
+    
+    print(data_files)
 
     # The file that the MME:s will be stored
     save_file_name = 'mme_table.txt'
@@ -47,16 +52,17 @@ def main():
     # Write what evaluation files are used as header
     save_file = open(save_file_name, 'a')
     save_file.write('Network type ')
-    for data_file in data_files
-        save_file.write(data_file + ' ')
-    save_file.write('\n')
+    for data_file in sorted(data_files):
+        save_file.write(str(data_file) + ' ')
+        save_file.write('\n')
     save_file.close()
 
     # Go through all evaluation files with the addback routine
-    save_file.open(save_file_name, 'a')
+    save_file = open(save_file_name, 'a')
     save_file.write('Addback ')
     save_file.close()
-    for data_file in data_files
+    for data_file in sorted(data_dir):
+        print('test')
         data, labels = load_data(data_file, total_portion=1, portion_zeros=PORTION_ZEROS)
 
         predictions, maxmult = addback(data, no_neighbors=1, energy_weighted=True, cluster=False)
@@ -66,58 +72,74 @@ def main():
         predictions, labels = get_permutation_match_with_permutations(predictions, labels)
         labels = reshapeArrayZeroPadding(labels, labels.shape[0], maxmult*3)
                 
-        MME = np.mean(get_momentum_error_dist(predictions, labels, False))
-        # MME_event = np.sum(get_momentum_error_dist(predictions, labels, False))/len(labels)
-        # MME = MME_event/3
-        save_file.open(save_file_name, 'a')
-        save_file.write(MME + ' ')
+        ME_dist = get_mse(predictions, labels, False)
+        MME = np.mean(ME_dist)
+        save_file = open(save_file_name, 'a')
+        save_file.write(str(MME) + ' ')
+        print('MME ' + str(MME))
         save_file.close()
+        eval_name1 = data_file
+        eval_name1 = eval_name1.replace('.npz','')
+        eval_name1 = eval_name1.replace('/','')
+        dist_addback = open('addback_' + eval_name1 + '_mme_dist.txt', 'a')
+        dist_addback.write(str(ME_dist))
+        dist_addback.close()       
     
     # Sweep through the FCNs
-    for weight_file in weight_files:  
-        # Go through all evaluation files for each FCN model
-        for data_file in data_files:
-            data, labels = load_data(data_file, total_portion=1, portion_zeros=PORTION_ZEROS)
-            no_inputs = len(data[0])
-            no_outputs = len(labels[0])
+    #for model_file in os.scandir(model_directory):
+    # if model_file.path.endswith(model_ext):
+    for model in sorted(os.listdir(model_directory)):
+        print(model)
+        if os.path.isdir(model)==False:
+            model = keras.models.load_model(model, compile=False)
+            save_file = open(save_file_name, 'a')
+            save_file.write('\n' + str(model_file) + ' ')
+            save_file.close()
         
-            # Optimized FCN
-            if network_type == NETWORK_TYPES['FCN']:
-                no_layers = 6 
-                no_nodes = 1200
-                model = FCN(no_inputs, no_outputs, no_layers, no_nodes)
+            # Go through all evaluation files for each FCN model
+            for data_file in sorted(data_dir):
+                data, labels = load_data(data_file, total_portion=1, portion_zeros=PORTION_ZEROS)
+                no_outputs = len(labels[0])
+                max_mult = int(no_outputs / 3)
+
+                # loss = LossFunction(max_mult, regression_loss='squared') 
+                loss = LossFunction(max_mult, regression_loss='absolute') 
         
-            # This needs to be updated
-            if network_type == NETWORK_TYPES['Dense_ResNet']:
-                no_nodes = 2**7
-                no_blocks = 10
-                model = Dense_ResNet(no_inputs, no_outputs, no_nodes, no_blocks, no_skipped_layers=3)
-        
-            max_mult = int(no_outputs / 3)
-            # loss = LossFunction(max_mult, regression_loss='squared') 
-            loss = LossFunction(max_mult, regression_loss='absolute') 
-            model.compile(optimizer=Adam(lr=LEARNING_RATE), loss=loss.get(), metrics=['accuracy'])
-        
-            model.load_weights(weight_file)
-
-            # Make reconstructions
-            predictions = model.predict(data)
+                # Make reconstructions
+                predictions = model.predict(data)
+                predictions, labels = get_permutation_match(predictions, labels, loss, max_mult)
     
-            predictions, labels = get_permutation_match(predictions, labels, loss, max_mult)
-    
-            #Check results
-            ME_dist = get_momentum_error_dist(predictions, labels, False)
-            print('Total momentum error = ', np.sum(ME_dist))
-            print('MME = ', np.mean(ME_dist))
+                #Check results
+                ME_dist = get_mse(predictions, labels, False)
+                MME = np.mean(ME_dist)
+                save_file = open(save_file_name, 'a')
+                save_file.write(str(MME)  + ' ')
+                print('\nMME ' + str(MME))
+                save_file.close()
+                eval_name = data_file
+                eval_name = eval_name.replace('.npz','')
+                eval_name = eval_name.replace('/','')
+                dist_ANN = open(model._name + '_' + str(model_file) + '_' + eval_name + '_mme_dist.txt', 'a')
+                dist_ANN.write(str(ME_dist))
+                dist_ANN.close()
+                print('Total momentum error = ', np.sum(ME_dist))
+                print('MME = ', str(MME))
+    print('\nDone')
 
-            # These are not needed for this script
-            #predictions = cartesian_to_spherical(predictions, error=False)
-            #labels = cartesian_to_spherical(labels, error=False)
+# Returns an array of mse/event
+def get_mse(prediction, label, spherical=True):
+    if spherical:
+        prediction = spherical_to_cartesian(prediction)
+        label = spherical_to_cartesian(label)
 
-    
-# if __name__ == "__main__":
-#     main() _angles=True) # ??
+    prediction_list = lambda q: prediction[::,q::3]
+    label_list = lambda q: label[::,q::3]
 
-    
+    momentum_tensor = np.array([prediction_list(0) - label_list(0), prediction_list(1) - label_list(1), prediction_list(2)- label_list(2)])
+
+    mult = int(prediction.shape[1]/3)
+    mse = [np.sum([np.linalg.norm(momentum_tensor[:,i,j]) for j in range(mult)]) for i in range(momentum_tensor.shape[1])]
+    return mse
+
 if __name__ == "__main__":
     main() 
